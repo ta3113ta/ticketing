6563275ab04a63fb1492cf937a0db4765ba44a66
Tabura <wuttichai3113@gmail.com>
2022-01-24 22:15:34 +0700

    146. Final Error Related Code

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,15 +1,16 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid request parameters");
 
     // Only because we are extending built in Error class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
-  
+
   serializeErrors() {
     return this.errors.map((err) => {
       return {
