0be037c5fad220356b07be91d93e9ee4fc72b83a
Tabura <wuttichai3113@gmail.com>
2022-01-27 23:11:31 +0700

    feat: add password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that describes the properties
-// that are required to create a new User
+// that are requried to create a new User
 interface UserAttrs {
   email: string;
   password: string;
@@ -13,7 +14,7 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-// An interface that descripes the properties
+// An interface that describes the properties
 // that a User Document has
 interface UserDoc extends mongoose.Document {
   email: string;
@@ -29,6 +30,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
