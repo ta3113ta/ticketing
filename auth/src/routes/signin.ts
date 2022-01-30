b8f28ef8812ecbe7bd77c127a450ccc25bd33237
Tabura <wuttichai3113@gmail.com>
2022-01-30 14:34:40 +0700

    182. Common Request Validation Middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 
-import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -11,13 +11,8 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password").trim().notEmpty().withMessage("Password is required"),
   ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-  }
+  validateRequest,
+  (req: Request, res: Response) => {}
 );
 
 export { router as signinRouter };
