bbbe1eee431f1f4fe5f6f0f14298febb6c5ab8e1
Tabura <wuttichai3113@gmail.com>
2022-02-01 23:33:43 +0700

    190. Requiring Auth for Route Access

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { requireAuth } from "../middlewares/require-auth";
 
 import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, (req, res) => {
+router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
