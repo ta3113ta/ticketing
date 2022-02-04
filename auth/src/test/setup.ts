1a07cf65988d7890113ab9af3bd22865b7590e02
Tabura <wuttichai3113@gmail.com>
2022-02-04 21:22:19 +0700

    207. Auth Helper Function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
@@ -24,3 +29,20 @@ beforeEach(async () => {
 afterAll(async () => {
   await mongo.stop();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
