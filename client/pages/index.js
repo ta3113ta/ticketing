2f18486c19e73d6a7164666d449bfac0af05a84f
Tabura <wuttichai3113@gmail.com>
2022-02-06 21:13:53 +0700

    fix: classify server or client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,17 @@
-const Landing = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // we are on the server!
+  } else {
+    // we are on the browser!
+  }
+
+  return {};
+};
+
+export default LandingPage;
