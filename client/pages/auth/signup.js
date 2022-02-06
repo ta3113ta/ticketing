ca2bf1e2f68408db0257733cb43dab9835dd3b99
Tabura <wuttichai3113@gmail.com>
2022-02-06 19:16:54 +0700

    fest: an onSuccess callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,26 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { doRequest , errors } = useRequest({
+  const { doRequest, errors } = useRequest({
     url: "/api/users/signup",
     method: "post",
     body: {
       email,
       password,
     },
+    onSuccess: () => {
+      Router.push("/");
+    },
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
   };
 
   return (
