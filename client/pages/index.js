8bf7ddd5094d6fe98e95f354ae2dbf346772dc90
Tabura <wuttichai3113@gmail.com>
2022-02-07 22:35:49 +0700

    fest: specifying Host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,27 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server!
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
   } else {
     // we are on the browser!
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
   }
 
   return {};
