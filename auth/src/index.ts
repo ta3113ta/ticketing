7ceea77f10107363a9c6b8374d9687ffd4241e79
Tabura <wuttichai3113@gmail.com>
2022-03-05 22:05:32 +0700

    fest: validating title and price

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,8 +7,12 @@ const start = async () => {
     throw new Error("JWT_KEY must be defind");
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongoDB");
   } catch (err) {
     console.error(err);
